ab1d2782efa8912d05a85018110cf65ef05a1b12
Niklas Becker <48069565+niklasbec@users.noreply.github.com>
2020-05-25 14:36:58 +0200

    done

--- add_two_numbers/add_two_numbers.js
+++ add_two_numbers/add_two_numbers.js
@@ -1,15 +1,50 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
+class LinkedListNode {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+let l1 = {}
+l1.head = new LinkedListNode(2)
+l1.head.next = new LinkedListNode(4)
+l1.head.next.next = new LinkedListNode(3)
+
+let l2 = {}
+l2.head = new LinkedListNode(5)
+l2.head.next = new LinkedListNode(6)
+l2.head.next.next = new LinkedListNode(4)
+
 var addTwoNumbers = function(l1, l2) {
-    
+    let arr1 = []
+    let arr2 = []
+    let total1 = ""
+    let total2 = ""
+    let total = 0
+    let currentL1 = l1.head;
+
+    while (currentL1 !== null) {
+        arr1.unshift(currentL1.data)
+        currentL1 = currentL1.next;
+    }
+
+    let currentL2 = l2.head;
+
+    while (currentL2 !== null) {
+        arr2.unshift(currentL2.data)
+        currentL2 = currentL2.next;
+    }
+
+    arr1.forEach(curr => {
+        total1 = total1 + curr.toString()
+    })
+    arr2.forEach(curr => {
+        total2 = total2 + curr.toString()
+    })
+
+    total = Number(total1) + Number(total2)
+
+    return total
 };
+
+console.log(addTwoNumbers(l1, l2))
--- contains-dupes/contains_dupe.js
+++ contains-dupes/contains_dupe.js
@@ -1,3 +1,31 @@
 var containsDuplicate = function(nums) {
-
+    let done =  []
+    let duplicates = 0
+    nums.forEach(curr => {
+        if(done.includes(curr)) {
+            duplicates = duplicates + 1
+        } else {
+            done.push(curr)
+        }
+    })
+    if(duplicates === 0) {
+        console.log(`${duplicates} duplicates found`)
+        return false
+    } else {
+        console.log(`${duplicates} duplicates found`)
+        return true
+    }
 }
+
+
+nums = [1, 2, 2, 3, 3, 4, 5, 6, 6, 7, 8, 9, 9, 20]
+nums2 = [1, 2, 3, ,4 ,5 ,6 ,7 , 8 ,9]
+
+console.log("-------------------------------")
+console.log("THIS SHOULD FIND DUPLICATES")
+console.log("-------------------------------")
+console.log(containsDuplicate(nums))
+console.log("-------------------------------")
+console.log("THIS SHOULDN'T FIND DUPLICATES")
+console.log("-------------------------------")
+console.log(containsDuplicate(nums2))
